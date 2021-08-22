6e196c6421a8c96529a55b235690e9ae54ef6721
Shammah <SE655@live.mdx.ac.uk>
2021-08-22 20:38:37 +0100

    Debugging Google Map with Provider & Firestore

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,6 +1,5 @@
 import 'dart:ui';
 
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:vendscape/providers/auth_provider.dart';
